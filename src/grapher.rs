f6f5cbc6a892501d9da03876a754e296bf07c931
Sam Sartor <me@samsartor.com>
2018-04-28 21:48:46 -0600

    range repitition and group expantions

--- src/grapher.rs
+++ src/grapher.rs
@@ -1,4 +1,5 @@
 use regex_syntax::ast::{self, Ast, parse::ParserBuilder};
+use regex_syntax::hir::{self, Hir, translate::TranslatorBuilder};
 use failure::Error;
 use petgraph::{self, prelude::*};
 use dot::{Node, Edge};
@@ -9,13 +10,54 @@ pub type Graph = petgraph::Graph<Node, Edge>;
 
 pub struct Grapher {
     pub parser: ParserBuilder,
+    pub trans: TranslatorBuilder,
 }
 
 impl Grapher {
     pub fn new() -> Grapher {
         Grapher {
             parser: ParserBuilder::new(),
+            trans: TranslatorBuilder::new(),
         }
+    }
+
+    pub fn ast_edge(&self, ast: &Ast) -> Edge {
+        Edge::new(format!("{}", ast))
+    }
+
+    pub fn try_hir_connect(
+        &self,
+        hir: &Hir,
+        left: Nodei,
+        right: Nodei,
+        machine: &mut Graph
+    ) -> Result<(), Option<Error>> {
+        use self::hir::HirKind::*;
+        match *hir.kind() {
+            Empty => (),
+            Literal(_) => {
+                machine.add_edge(left, right, Edge::new(format!("{}", hir)));
+            },
+            Alternation(ref hirs) => for h in hirs {
+                self.try_hir_connect(h, left, right, machine)?
+            },
+            Class(hir::Class::Unicode(ref ls)) => {
+                let mut chars = Vec::new();
+                for range in ls.iter() {
+                    for c in u32::from(range.start())..=u32::from(range.end()) {
+                        if let Some(c) = ::std::char::from_u32(c) {
+                            chars.push(c);
+                            if chars.len() > 4 { return Err(None) }
+                        }
+                    }
+                }
+                for c in chars {
+                    machine.add_edge(left, right, Edge::new(format!("{}", c)));
+                }
+            },
+            _ => return Err(None),
+        }
+        Ok(())
     }
 
     pub fn connect(
@@ -50,7 +92,7 @@ impl Grapher {
             },
             Group(ref gr) => self.connect(&gr.ast, left, right, machine)?,
             Repetition(ref rep) => {
-                use self::ast::RepetitionKind::*;
+                use self::ast::{RepetitionKind::*, RepetitionRange::*};
                 match rep.op.kind {
                     ZeroOrOne => {
                         machine.add_edge(left, right, Edge::Epsilon);
@@ -70,11 +112,44 @@ impl Grapher {
                         machine.add_edge(m, right, Edge::Epsilon);
                         self.connect(&rep.ast, m, m, machine)?;
                     },
-                    _ => bail!("unsupported repetition"),
+                    Range(ref range) => {
+                        let base = match *range {
+                            Exactly(n) => n,
+                            AtLeast(n) => n,
+                            Bounded(n, _) => n,
+                        };
+                        let mut at = left;
+                        for _ in 0..base {
+                            let next = machine.add_node(Node::mid());
+                            self.connect(&rep.ast, at, next, machine)?;
+                            at = next;
+                        }
+                        match range {
+                            AtLeast(_) => self.connect(&rep.ast, at, at, machine)?,
+                            Bounded(min, max) if max >= min => {
+                                for _ in 0..(max - min) {
+                                    machine.add_edge(at, right, Edge::Epsilon);
+                                    let next = machine.add_node(Node::mid());
+                                    self.connect(&rep.ast, at, next, machine)?;
+                                    at = next;
+                                }
+                            },
+                            _ => (),
+                        }
+                        machine.add_edge(at, right, Edge::Epsilon);
+                    },
                 }
             },
+            Assertion(_) => (),
             _ => {
-                machine.add_edge(left, right, Edge::new(format!("{}", ast)));
+                let hir = self.trans.build().translate("" /* TODO: need source */, ast)?;
+                match self.try_hir_connect(&hir, left, right, machine) {
+                    Ok(()) => (),
+                    Err(None) => {
+                        machine.add_edge(left, right, self.ast_edge(ast));
+                    },
+                    Err(Some(e)) => return Err(e),
+                }
             },
         }
 
